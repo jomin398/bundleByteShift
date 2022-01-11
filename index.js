fcf89ae929c316c60e16812cb7bf16520efba97b
jomin398 <jomin398@gmail.com>
2022-01-11 23:01:47 +0900

    update

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ var main;
                 url: null
             };
         };
+        mylog(str, delLast) {
+            let consoleView = document.body.querySelector('ul');
+            if (!delLast) {
+                consoleView.appendChild((() => {
+                    let p = document.createElement('li');
+                    p.innerText = str;
+                    return p;
+                })());
+            } else {
+                consoleView.lastChild.innerText = str;
+            }
+        };
         init() {
             this.form = document.querySelector('form[name=input]');
             if (this.seviceMulti) {
@@ -27,8 +39,65 @@ var main;
             (new Uint8Array(buffer)).forEach((v) => { s += h[v >> 4] + h[v & 15]; });
             return s;
         }
-        process() {
+        onLoadFile(curFiles) {
+            this.mylog('> onLoad...');
+            for (const file of curFiles) {
+                var dec = new TextDecoder("utf-8");
+                var fileReader = new FileReader();
+                var size = file.size;
+                var name = file.name;
+                var offset = this.value.offset;
+                var self = this;
+                var chunk_size = Math.pow(2, 4); //16
+                fileReader.readAsArrayBuffer(file);
+                fileReader.onload = function (e) {
+                    self.mylog('> onLoad...done');
 
+                    self.mylog(`> File name is "${name}".`);
+                    self.mylog(`> File size is ${size} bytes.`);
+                    self.mylog(`> file offset is ${offset} bytes.`);
+                    self.value.data = e.target.result.slice(offset);
+                    self.value.dataB = e.target.result.slice(0, chunk_size - 1);
+                    self.value.dataA = self.value.data.slice(0, chunk_size - 1);
+                    self.mylog(`> origin of ${chunk_size} bytes of fileData :"${dec.decode(self.value.dataB)}"`);
+                    self.mylog(`> preview of ${chunk_size} bytes of edited fileData :"${dec.decode(self.value.dataA)}"`);
+                    //console.log(self.value.data); // ArrayBuffer 객체
+                    const mergedBlob = new Blob(
+                        [self.value.data],
+                        { type: 'application/octet-stream' }
+                    );
+                    self.value.url = window.URL.createObjectURL(mergedBlob);
+
+                    console.log(self.value.url)
+                    document.body.append((() => {
+                        let b = document.createElement('button');
+                        b.innerText = 'download';
+                        b.onclick = () => {
+                            var a = document.createElement("a")
+                            a.innerText = 'download';
+                            a.href = self.value.url;
+                            a.download = name;
+                            document.body.appendChild(a);
+                            a.click();
+                            setTimeout(function () {
+                                document.body.removeChild(a);
+                                //window.URL.revokeObjectURL(url);
+                            }, 0);
+                        }
+                        return b;
+                    })(), document.createElement('br'), (() => {
+                        let b = document.createElement('button');
+                        b.innerText = 'Page Reload';
+                        b.onclick = () => {
+                            window.URL.revokeObjectURL(self.value.url);
+                            location.reload();
+                        };
+                        return b;
+                    })());
+
+
+                }
+            }
         }
         submitHandler() {
             // console.log(this.form)
@@ -37,70 +106,10 @@ var main;
             this.form.style.display = 'none';
             const consoleView = document.createElement('ul');
             document.body.appendChild(consoleView);
-            const mylog = (str, delLast) => {
-                if (!delLast) {
-                    consoleView.appendChild((() => {
-                        let p = document.createElement('li');
-                        p.innerText = str;
-                        return p;
-                    })());
-                } else {
-                    consoleView.lastChild.innerText = str;
-                }
-            }
             if (curFiles.length === 0) {
-                mylog('> No files currently selected for upload');
+                this.mylog('> No files currently selected for upload');
             } else {
-                mylog('> onLoad...');
-                var chunk_size = Math.pow(2, 4); //16
-                for (const file of curFiles) {
-                    var dec = new TextDecoder("utf-8");
-                    var fileReader = new FileReader();
-                    var size = file.size;
-                    var name = file.name;
-                    var offset = this.value.offset;
-                    var self = this;
-                    fileReader.readAsArrayBuffer(file);
-                    fileReader.onload = function (e) {
-                        mylog('> onLoad...done');
-
-                        mylog(`> File name is "${name}".`);
-                        mylog(`> File size is ${size} bytes.`);
-                        mylog(`> file offset is ${offset} bytes.`);
-                        self.value.data = e.target.result.slice(offset);
-                        self.value.dataB = e.target.result.slice(0, chunk_size - 1);
-                        self.value.dataA = self.value.data.slice(0, chunk_size - 1);
-                        mylog(`> origin of ${chunk_size} bytes of fileData :"${dec.decode(self.value.dataB)}"`);
-                        mylog(`> preview of ${chunk_size} bytes of fileData :"${dec.decode(self.value.dataA)}"`);
-                        //console.log(self.value.data); // ArrayBuffer 객체
-                        const mergedBlob = new Blob(
-                            [self.value.data],
-                            { type: 'application/octet-stream' }
-                        );
-                        self.value.url = window.URL.createObjectURL(mergedBlob);
-
-                        console.log(self.value.url)
-                        document.body.appendChild(() => {
-                            let b = document.createElement('button');
-                            b.innerText = 'download';
-                            b.onclick = () => {
-                                var a = document.createElement("a")
-                                a.innerText = 'download';
-                                a.href = self.value.url;
-                                a.download = name;
-                                document.body.appendChild(a);
-                                a.click();
-                                setTimeout(function () {
-                                    document.body.removeChild(a);
-                                    //window.URL.revokeObjectURL(url);
-                                }, 0);
-                            }
-                            return b;
-                        })();
-
-
-                    }
-                }
+                this.onLoadFile(curFiles);
             }
         }
     }
---
+++ readme.md
@@ -0,0 +1,23 @@
+# bundle Byte Shift
+Unity Asset `.bundle` file Byte Shifter   
+as name as **UABS** for short.    
+for **Debuging** Unity Asset Bundle file or else.   
+This project is licensed as **MIT License**.
+
+![HTMLLogo](https://shields-staging.herokuapp.com/static/v1?message=HTML5&label&color=black&style=for-the-badge&logo=HTML5)
+![UnityLogo](https://shields-staging.herokuapp.com/static/v1?message=Unity&label&color=black&style=for-the-badge&logo=unity)
+
+# Table of contents
+1. [How to Use](#how-to-use)
+2. [Todo](#todo)
+
+# How to Use
+1. input a **Number** for byte shift. **(Optional)**
+2. input a `.bundle` file.
+3. press **Submit**
+4. press **Download**
+5. press **Page Reload** for page Reload to restart whole again.
+
+### TODO
+- [x] main function.
+- [ ] display hex on table. (addtion)
